0860f99a62a469ba2b602c479c31708cbe83d866
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-14 11:38:50 +0530

    Update app.tsx

--- apps/pwa/src/app/app.tsx
+++ apps/pwa/src/app/app.tsx
@@ -1,10 +1,19 @@
-import NxWelcome from './nx-welcome';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { askForPermissionToReceiveNotifications } from '../firebase';
-import styles from './app.module.css';
+import {
+  askForPermissionToReceiveNotifications,
+  onMessageListener,
+} from '../firebase';
+
+import NxWelcome from './nx-welcome';
 
 export function App() {
   askForPermissionToReceiveNotifications();
+
+  onMessageListener()
+    .then((payload: any) => {
+      alert(payload);
+    })
+    .catch((err) => console.log('failed: ', err));
 
   return (
     <>
