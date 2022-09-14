a2325067bf4adcde1cc1787ee9cce358fde3ca6e
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-14 13:08:53 +0530

    new build

--- apps/pwa/src/firebase.ts
+++ apps/pwa/src/firebase.ts
@@ -15,28 +15,32 @@ const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
 export const askForPermissionToReceiveNotifications = (setToken: any) => {
-  return getToken(messaging, {
-    vapidKey:
-      'BFffdqLbxNEDVTEQC_pHfvep4wnJ6fbwaFJnojs3cjxqCTV-Y7L8Zp5M-wHFPrhmUWap4pEheciah-xluB6Iobk',
-  })
-    .then((currentToken) => {
-      if (currentToken) {
-        setToken(currentToken);
-        console.log('current token for client: ', currentToken);
-        // Track the token -> client mapping, by sending to backend server
-        // show on the UI that permission is secured
-      } else {
-        console.log(
-          'No registration token available. Request permission to generate one.'
-        );
-        // shows on the UI that permission is required
-      }
+  try {
+    return getToken(messaging, {
+      vapidKey:
+        'BFffdqLbxNEDVTEQC_pHfvep4wnJ6fbwaFJnojs3cjxqCTV-Y7L8Zp5M-wHFPrhmUWap4pEheciah-xluB6Iobk',
     })
-    .catch((err) => {
-      setToken(err);
-      console.log('An error occurred while retrieving token. ', err);
-      // catch error while creating client token
-    });
+      .then((currentToken) => {
+        if (currentToken) {
+          setToken(currentToken);
+          console.log('current token for client: ', currentToken);
+          // Track the token -> client mapping, by sending to backend server
+          // show on the UI that permission is secured
+        } else {
+          console.log(
+            'No registration token available. Request permission to generate one.'
+          );
+          // shows on the UI that permission is required
+        }
+      })
+      .catch((err) => {
+        setToken(err);
+        console.log('An error occurred while retrieving token. ', err);
+        // catch error while creating client token
+      });
+  } catch (ex: any) {
+    return 'fake token' + ex.toString();
+  }
 };
 
 export const onMessageListener = () =>
