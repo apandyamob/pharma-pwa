61de7444a64a3cacb58f3f81d66bc3b9f678f5da
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-14 13:45:20 +0530

    added boostrap

--- apps/pwa/src/app/app.tsx
+++ apps/pwa/src/app/app.tsx
@@ -1,29 +1,67 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button, Toast } from 'react-bootstrap';
 import {
   askForPermissionToReceiveNotifications,
   onMessageListener,
 } from '../firebase';
 
-import NxWelcome from './nx-welcome';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import logo from '../assets/icons/logo.png';
 import { useState } from 'react';
 
-export function App() {
-  const [token, setToken] = useState('');
-  askForPermissionToReceiveNotifications(setToken);
+export default function App() {
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({ title: '', body: '' });
+  const [isTokenFound, setTokenFound] = useState(false);
+  askForPermissionToReceiveNotifications(setTokenFound);
 
-  // onMessageListener()
-  //   .then((payload: any) => {
-  //     alert(payload);
-  //   })
-  //   .catch((err) => console.log('failed: ', err));
+  onMessageListener()
+    .then((payload: any) => {
+      setNotification({
+        title: payload.notification.title,
+        body: payload.notification.body,
+      });
+      setShow(true);
+      console.log(payload);
+    })
+    .catch((err: any) => console.log('failed: ', err));
+
+  const onShowNotificationClicked = () => {
+    setNotification({
+      title: 'Notification',
+      body: 'This is a test notification',
+    });
+    setShow(true);
+  };
 
   return (
-    <>
-      TOKEN: {token}
-      <NxWelcome title="pwa" />
-      <div />
-    </>
+    <div className="App">
+      <Toast
+        onClose={() => setShow(false)}
+        show={show}
+        delay={3000}
+        autohide
+        animation
+        style={{
+          position: 'absolute',
+          top: 20,
+          right: 20,
+          minWidth: 200,
+        }}
+      >
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+          <strong className="mr-auto">{notification.title}</strong>
+          <small>just now</small>
+        </Toast.Header>
+        <Toast.Body>{notification.body}</Toast.Body>
+      </Toast>
+      <header className="App-header">
+        {isTokenFound && <h1> Notification permission enabled 👍🏻 </h1>}
+        {!isTokenFound && <h1> Need notification permission ❗️ </h1>}
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button onClick={() => onShowNotificationClicked()}>Show Toast</Button>
+      </header>
+    </div>
   );
 }
-
-export default App;
