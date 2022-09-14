5c047738fea2b2fbb152c6e4b984f46bbd7e95ea
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-14 15:39:44 +0530

    Update firebase-messaging-sw.js

--- apps/pwa/src/firebase-messaging-sw.js
+++ apps/pwa/src/firebase-messaging-sw.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
+importScripts(
+  'https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js'
+);
 importScripts(
   'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js'
-)
+);
 
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
@@ -14,20 +16,21 @@ var firebaseConfig = {
   storageBucket: 'pharmacy-pwa.appspot.com',
   messagingSenderId: '490787804854',
   appId: '1:490787804854:web:dee991a03e221487a4f817',
-}
+};
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
-const messaging = firebase.messaging()
+const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
-  console.log('Received background message ', payload)
+  console.log('Received background message ', payload);
 
-  const notificationTitle = payload.notification.title
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-  }
+    tag: 'notification-1',
+  };
   console.log('I am from service worker');
-  self.registration.showNotification(notificationTitle, notificationOptions)
-})
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
