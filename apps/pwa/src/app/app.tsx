2cf979e825346e25a9f74f6fe941759804a32b3d
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-14 17:22:06 +0530

    Update app.tsx

--- apps/pwa/src/app/app.tsx
+++ apps/pwa/src/app/app.tsx
@@ -11,25 +11,56 @@ import { useState } from 'react';
 
 export default function App() {
   const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({ title: '', body: '' });
   const [token, setToken] = useState(null);
   askForPermissionToReceiveNotifications(setToken);
 
   onMessageListener()
     .then((payload: any) => {
-      new Notification(payload.notification.title, payload.notification.body);
+      setNotification({
+        title: payload.notification.title,
+        body: payload.notification.body,
+      });
+
+      new Notification(payload.notification.title, {
+        body: payload.notification.body,
+      });
+
       setShow(true);
       console.log(payload);
     })
     .catch((err: any) => console.log('failed: ', err));
 
   const onShowNotificationClicked = () => {
-    new Notification('Sample notification', { body: 'Hi there' });
-
+    setNotification({
+      title: 'Notification',
+      body: 'This is a test notification',
+    });
+    new Notification('Notification', { body: 'This is a test notification' });
     setShow(true);
   };
 
   return (
     <div className="App">
+      <Toast
+        onClose={() => setShow(false)}
+        show={show}
+        delay={10000}
+        autohide
+        animation
+        style={{
+          position: 'absolute',
+          top: 20,
+          right: 20,
+          minWidth: 200,
+        }}
+      >
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+          <strong className="mr-auto">{notification.title}</strong>
+        </Toast.Header>
+        <Toast.Body>{notification.body}</Toast.Body>
+      </Toast>
       <header className="App-header">
         {token && <h1> Notification permission enabled 👍🏻 </h1>}
         {!token && <h1> Need notification permission ❗️ </h1>}
