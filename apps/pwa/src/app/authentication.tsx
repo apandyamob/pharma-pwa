94a4e3d03a5a8e36170e96256452a1c41417c590
Jigar Shah <jigars3@mobiquityinc.com>
2023-03-07 16:34:22 +0530

    0703 2.3 PWA changes

--- apps/pwa/src/app/authentication.tsx
+++ apps/pwa/src/app/authentication.tsx
@@ -122,7 +122,7 @@ export default function Authentication() {
     <>
       <DeviceDetails />
       <button id="register" onClick={registerCredential}>
-        Register Credential 2.2
+        Register Credential 2.3
       </button>
 
       <br />
