cdd46142a71ccc6888cc0ebd85c47eb2f5fb69e8
Jigar Shah <jigars3@mobiquityinc.com>
2023-03-09 14:25:21 +0530

    0703 4.2 PWA full fixed removed listener test

--- apps/pwa/src/app/authentication.tsx
+++ apps/pwa/src/app/authentication.tsx
@@ -122,7 +122,7 @@ export default function Authentication() {
     <>
       <DeviceDetails />
       <button id="register" onClick={registerCredential}>
-        Register Credential 4.1
+        Register Credential 4.2
       </button>
 
       <br />
