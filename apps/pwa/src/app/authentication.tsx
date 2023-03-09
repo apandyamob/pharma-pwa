2cdfd63707be23996936f2a8dbca5aae38ab8b05
Jigar Shah <jigars3@mobiquityinc.com>
2023-03-09 14:04:42 +0530

    0703 4.0 PWA full fixed skip test

--- apps/pwa/src/app/authentication.tsx
+++ apps/pwa/src/app/authentication.tsx
@@ -122,7 +122,7 @@ export default function Authentication() {
     <>
       <DeviceDetails />
       <button id="register" onClick={registerCredential}>
-        Register Credential 3.9
+        Register Credential 4.0
       </button>
 
       <br />
