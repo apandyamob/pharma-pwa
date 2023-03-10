463e4f109ba9c3589b39b2f7fa1232e373ee36b3
Jigar Shah <jigars3@mobiquityinc.com>
2023-03-10 15:57:52 +0530

    1003 5.8 PWA full added controllerchange and removed old reload logic test

--- apps/pwa/src/app/authentication.tsx
+++ apps/pwa/src/app/authentication.tsx
@@ -124,7 +124,7 @@ export default function Authentication() {
     <>
       <DeviceDetails />
       <button id="register" onClick={registerCredential}>
-        Register Credential 5.7
+        Register Credential 5.8
       </button>
 
       <br />
