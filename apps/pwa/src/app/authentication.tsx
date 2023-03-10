dc7402e08c4117e7ca3f8a47a74cd6e8cfab06e1
Jigar Shah <jigars3@mobiquityinc.com>
2023-03-10 12:38:26 +0530

    0703 5.4 PWA full added waiting worker test

--- apps/pwa/src/app/authentication.tsx
+++ apps/pwa/src/app/authentication.tsx
@@ -124,7 +124,7 @@ export default function Authentication() {
     <>
       <DeviceDetails />
       <button id="register" onClick={registerCredential}>
-        Register Credential 5.3
+        Register Credential 5.4
       </button>
 
       <br />
