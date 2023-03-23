a6fad6b66cd9caf0ea33bd416f95e161e73c8a06
Jigar Shah <jigars3@mobiquityinc.com>
2023-03-23 11:48:10 +0530

    changes for localstorage alert and reduced interval 6.2 test

--- apps/pwa/src/registerServiceWorker.ts
+++ apps/pwa/src/registerServiceWorker.ts
@@ -44,14 +44,14 @@ const registerServiceWorker = () => {
       function (registration) {
         console.log('Service worker registration succeeded:', registration);
         // below condition will ensure that confirm modal not shown for the first installation
-        if (!navigator.serviceWorker.controller) {
-          console.log('controller for first time');
-          // The window client isn't currently controlled so it's a new service
-          // worker that will activate immediately
-          return;
-        }
+        // if (!navigator.serviceWorker.controller) {
+        // console.log('controller for first time');
+        // The window client isn't currently controlled so it's a new service
+        // worker that will activate immediately
+        // return;
+        // }
 
-        // registration.update();
+        registration.update();
 
         setInterval(() => {
           registration.update();
@@ -60,7 +60,7 @@ const registerServiceWorker = () => {
             registration.installing,
             registration.waiting
           );
-        }, 1000 * 60 * 0.5); // 1 min
+        }, 1000 * 60 * 0.1); // 1 min
 
         registration.onupdatefound = () => {
           const installingServiceWorker = registration.installing;
